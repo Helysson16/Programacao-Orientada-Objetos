6c5ea6aeba9ccad68cf02b70d4eee6897dea83f6
Helysson16 <115591449+Helysson16@users.noreply.github.com>
2023-06-13 09:24:46 -0300

    Add files via upload

---
+++ [POO- Manhã] [Helysson e Rilary] [Atividade Elevador]/Classes.js
@@ -0,0 +1,57 @@
+class Passageiro {
+    constructor(andarpretendido, peso) {
+        this.andarpretendido = andarpretendido;
+        this.peso = peso
+    }
+}
+class Elevador {
+    constructor(pesoMaximo) {
+        this.andarAtual = 0
+        this.pesoMaximoSuportado = pesoMaximo
+        this.listaDePassageiro = []
+    }
+    adicionarPassageiro(passageiro) {
+        let somaDePesos = 0
+        console.log(this.listaDePassageiro.length);
+        for (let index = 0; index < this.listaDePassageiro.length; index++) {
+            somaDePesos += this.listaDePassageiro[index].peso
+        }
+        console.log(somaDePesos);
+
+        if (this.pesoMaximoSuportado > somaDePesos + passageiro.peso) {
+
+            this.listaDePassageiro.push(passageiro)
+            console.log(`1 passageiro que deseja ir ao andar ${passageiro.andarpretendido} ENTROU no elevador.
+                    \n Agora o elevador tem ${this.listaDePassageiro.length} passageiros`)
+
+        }
+        else {
+            console.log(`O passageiro não pode entrar. O elevador já atingiu a capacidade máxima de pesos: 
+                    ${this.pesoMaximoSuportado}`)
+        }
+
+    }
+
+    movimentar(andarDeDestino) {
+        this.andarAtual = andarDeDestino;
+        let pessoas = 0
+        console.log(`Agora o elevador está no ANDAR ${this.andarAtual}`);
+        for (var i = 0; i < this.listaDePassageiro.length; i++) {
+            if (this.listaDePassageiro[i].andarpretendido == this.andarAtual) {
+                this.listaDePassageiro.splice(i, 1);
+                pessoas++;
+            }
+
+        }
+        console.log(`${pessoas} passageiros saíram do elevador. \n
+            ${this.listaDePassageiro.length} continuam no elevador`)
+        for (var i = 0; i < this.listaDePassageiro.length; i++) {
+            console.log(`${this.listaDePassageiro[i]}`)
+
+        }
+
+    }
+
+}
+
+
